fcd00c90bf4ca5eedf92977f8652399b220d8e7f
tragomani <144413592+tragomani@users.noreply.github.com>
2023-09-15 13:17:29 +0530

    Add files via upload

---
+++ Challenge unit-2/main.py
@@ -0,0 +1,30 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Amount must be greater than zero.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient funds.")
+
+    def display_balance(self):
+        print(f"Account Balance for {self.__account_holder_name}: ${self.__account_balance}")
+
+
+account = BankAccount("1432", "trago", 1987)
+
+account.display_balance()
+account.deposit(500)
+account.withdraw(200)
+account.display_balance()
