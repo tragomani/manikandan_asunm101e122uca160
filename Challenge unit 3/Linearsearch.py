a77b43960b1c7ef4f8adda3db5c1f36955b93ff6
tragomani <144413592+tragomani@users.noreply.github.com>
2023-10-11 11:35:22 +0530

    Update Linearsearch.py

--- Challenge unit 3/Linearsearch.py
+++ Challenge unit 3/Linearsearch.py
@@ -1,7 +1,17 @@
-def linear_search_product(products, target_product):
+def linear_search_product(product_list, target_product):
     indices = []
-    for i in range(len(products)):
-        if products[i] == target_product:
+    for i, product in enumerate(product_list):
+        if product == target_product:
             indices.append(i)
     return indices
-  
+
+product_list = ["Apple", "Banana", "Orange", "Apple", "Grapes", "Apple"]
+
+target_product = "Banana"
+
+result = linear_search_product(product_list, target_product)
+
+if result:
+    print("Indices of", target_product, ":", result)
+else:
+    print(target_product, "not found in the product list.")
