abc342a953f79fe7c9902ee32fd628c7b197f653
tragomani <144413592+tragomani@users.noreply.github.com>
2023-10-11 11:36:30 +0530

    Update Sort.py

--- Challenge unit 3/Sort.py
+++ Challenge unit 3/Sort.py
@@ -7,3 +7,15 @@ class Student:
 def sort_students(student_list):
     sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
     return sorted_students
+
+students = [
+    Student("Alice", "101", 3.9),
+    Student("Bob", "102", 3.7),
+    Student("Charlie", "103", 5.0),
+    Student("David", "104", 4.0),
+]
+
+sorted_students = sort_students(students)
+
+for student in sorted_students:
+    print(f"Name: {student.name}, Roll Number: {student.roll_number}, CGPA: {student.cgpa}"
